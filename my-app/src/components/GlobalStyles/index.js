356f6ae7b455707421861664101a5f462da3bae8
KhoiNNguyen <121741308+KhoiNNguyen@users.noreply.github.com>
2023-11-23 10:19:13 +0700

    Layout
    Tạo header slideShow footer

---
+++ my-app/src/components/GlobalStyles/index.js
@@ -0,0 +1,8 @@
+import styles from './GlobalStyles.scss'
+
+
+function GlobalStyles({ children }) {
+    return children;
+}
+
+export default GlobalStyles;
---
+++ my-app/src/components/Slide/index.js
@@ -0,0 +1,34 @@
+import { useState } from 'react';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Carousel from 'react-bootstrap/Carousel';
+import classNames from 'classnames/bind';
+
+import styles from './Slide.modual.scss'
+import { eventWrapper } from '@testing-library/user-event/dist/utils';
+
+const cx=classNames.bind(styles)
+
+function SlideShow() {
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex) => {
+    setIndex(selectedIndex);
+  };
+
+  return (
+    <Carousel activeIndex={index} onSelect={handleSelect} className={cx('carousel-index')} >
+      <Carousel.Item>
+        <img src="https://cdn.hoanghamobile.com/i/home/Uploads/2023/11/20/1200x375-vivo-201123.jpg" alt="Banner Big Redmi 12" />  
+      </Carousel.Item>
+      <Carousel.Item>
+        <img src="https://cdn.hoanghamobile.com/i/home/Uploads/2023/11/18/web-htc-wildfire-e3-lite-02.jpg" alt="Banner Big Redmi 12" />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img src="https://cdn.hoanghamobile.com/i/home/Uploads/2023/11/15/web-dong-ho-kieslect-01.jpg" alt="Banner Big Redmi 12" />
+      </Carousel.Item>
+    </Carousel>
+  );
+}
+
+export default SlideShow;
