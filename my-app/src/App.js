6272e44b1d584920064be841936aa6dccbc7da4c
KhoiNNguyen <121741308+KhoiNNguyen@users.noreply.github.com>
2023-11-27 08:37:51 +0700

    Sua header/footer

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,18 +1,17 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
-import Header from "./layouts/Header"
+import Layout from "./pages/Layout"
+import Home from "./pages/Home"
 
 const App = () =>{
     return(
-        // <BrowserRouter>
-        //     <Routes>
-        //         <Route path="/" element={<Layout/>}>
-        //             <Route index element={<Home/>}/>
-        //             <Route path="news" element={<News/>}/>
-        //             <Route path="contact" element={<Contact />}/>
-        //         </Route>
-        //     </Routes>
-        // </BrowserRouter>
-        <Header />
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    <Route index element={<Home />}/>
+                    
+                </Route>
+            </Routes>
+        </BrowserRouter>
     )
 }
 export default App
